e7b41d8b1b8f022d890f8a2a95ce8f5c7800301a
fitzgebt <fitzgebt@gmail.com>
2021-03-29 09:29:08 -0700

    added nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   resources :pieces
   resources :museums
-  resources :artists
+  # resources :artists
+  resources :artists do
+    resources :piecesg, only: [:show, :index]
+  end
+  root 'static#home'
+  get '/signin', to: 'session#new', as: 'signin'
+  post '/session', to: 'session#create', as: 'session'
+  delete '/session/', to: 'session#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
