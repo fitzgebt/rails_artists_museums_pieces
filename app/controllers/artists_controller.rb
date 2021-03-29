b75bfbea201f911f17648550ec990a5041202c5b
fitzgebt <fitzgebt@gmail.com>
2021-03-29 11:16:53 -0700

    began defining ArtistController actions

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,18 +1,28 @@
 class ArtistsController < ApplicationController
 
     def index
+        @artists = Artist.all
     end
 
     def new
+        @artist = Artist.new
     end
 
     def create
+        if (artist = Artist.create(artist_params))
+            session[:artist_id] = artist.id
+            redirect_to artist_path(artist)
+        else
+            render 'new'
+        end
     end
 
     def show
+        @artist = Artist.find_by(id: params[:id])
     end
 
     def edit
+        @artist = Artist.find_by(id: params[:id])
     end
 
     def update
