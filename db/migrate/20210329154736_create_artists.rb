02d95a04fa392c2c33a7530c04c1d14a798edd65
fitzgebt <fitzgebt@gmail.com>
2021-03-29 08:48:01 -0700

    generaated resources for Artist

---
+++ db/migrate/20210329154736_create_artists.rb
@@ -0,0 +1,12 @@
+class CreateArtists < ActiveRecord::Migration[5.2]
+  def change
+    create_table :artists do |t|
+      t.string :username
+      t.string :name
+      t.string :password_digest
+      t.string :hometown
+
+      t.timestamps
+    end
+  end
+end
