cbbe19adaed94123c665db6f97c0dec12980cb05
fitzgebt <fitzgebt@gmail.com>
2021-03-30 12:55:17 -0700

    added #make_art for Piece create message

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,4 +2,17 @@ class Piece < ApplicationRecord
     belong_to :museum
 	belong_to :artist
 
+
+    def make_art
+        if self.artist.pieces.count == 1
+            "Congrats, you are now an artist."
+        elsif self.artist.pieces.count <= 1 && self.artist.pieces.count >= 5
+            "Another masterpiece created. You are really VanGogh-ing somewhere."
+        elsif  self.artist.pieces.count < 5
+            "Another masterpiece created. You are the pinacle of artistic abilities."
+        end
+    end
+
+
+
 end
