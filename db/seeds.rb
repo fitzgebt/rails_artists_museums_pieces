1512146060f3be3436008dbc213d201be5e74b9c
fitzgebt <fitzgebt@gmail.com>
2021-04-03 18:26:43 -0700

    Tested app in current state, all routes, links working correctly. Began seed file work.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,167 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+museum_hash = {
+    "museum_1" => {
+        name: "Louvre",
+        location: "Paris, FR",
+        open_status: false
+    },
+    "museum_2" => {
+        name: ,
+        location: ,
+        open_status: false
+    },
+    "museum_3" => {
+        name: ,
+        location: ,
+        open_status: false
+    },
+    "museum_4" => {
+        name: ,
+        location: ,
+        open_status: false
+    },
+    "museum_5" => {
+        name: ,
+        location: ,
+        open_status: false
+    },
+}
+
+artist_hash = {
+    "artist_1" => {
+        name: "Pablo Picasso",
+        username: "picassop",
+        hometown: "Spain",
+        password: SecureRandom.hex
+    },
+    "artist_2" => {
+        name: "Vincent van Gogh",
+        username: "vangoghv",
+        hometown: " Zundert, Netherlands",
+        password: SecureRandom.hex
+    },
+    "artist_3" => {
+        name: "Leonardo da Vinci",
+        username: "davincil",
+        hometown: "Anchiano, Italy",
+        password: SecureRandom.hex
+    },
+    "artist_4" => {
+        name: "Michelangelo",
+        username: "theMichelangelo",
+        hometown: "Rome, Italy",
+        password: SecureRandom.hex
+    },
+    "artist_5" => {
+        name: "Henri Matisse",
+        username: "matisseh",
+        hometown: "Le Cateau-Cambrésis, France",
+        password: SecureRandom.hex
+    },
+    "artist_6" => {
+        name: "Jackson Pollock",
+        username: "pollockj",
+        hometown: "Cody, WY",
+        password: SecureRandom.hex
+    },
+    "artist_7" => {
+        name: "Claude Monet",
+        username: "showmethemonet",
+        hometown: "Rue Laffitte, Paris, France",
+        password: SecureRandom.hex
+    }
+}
+
+pieces_hash = {
+    piece_1 => {
+        name: ,
+        year_created: ,
+        image: ,
+        image_description: ,
+        artist_id: ,
+        museum_id: 
+    },
+    piece_2 => {
+        name: ,
+        year_created: ,
+        image: ,
+        image_description: ,
+        artist_id: ,
+        museum_id: 
+    },
+    piece_3 => {
+        name: ,
+        year_created: ,
+        image: ,
+        image_description: ,
+        artist_id: ,
+        museum_id: 
+    },
+    piece_4 => {
+        name: ,
+        year_created: ,
+        image: ,
+        image_description: ,
+        artist_id: ,
+        museum_id: 
+    },
+    piece_5 => {
+        name: ,
+        year_created: ,
+        image: ,
+        image_description: ,
+        artist_id: ,
+        museum_id: 
+    },
+    piece_6 => {
+        name: ,
+        year_created: ,
+        image: ,
+        image_description: ,
+        artist_id: ,
+        museum_id: 
+    },
+    piece_7 => {
+        name: ,
+        year_created: ,
+        image: ,
+        image_description: ,
+        artist_id: ,
+        museum_id: 
+    },
+    piece_8 => {
+        name: ,
+        year_created: ,
+        image: ,
+        image_description: ,
+        artist_id: ,
+        museum_id: 
+    },
+    piece_9 => {
+        name: ,
+        year_created: ,
+        image: ,
+        image_description: ,
+        artist_id: ,
+        museum_id: 
+    },
+    piece_10 => {
+        name: ,
+        year_created: ,
+        image: ,
+        image_description: ,
+        artist_id: ,
+        museum_id: 
+    },
+    piece_11 => {
+        name: ,
+        year_created: ,
+        image: ,
+        image_description: ,
+        artist_id: ,
+        museum_id: 
+    }
+}
